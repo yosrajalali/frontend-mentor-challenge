69d7d0f174604ac6f967df2d6ae66e65bb16771d
yosrajalali <100087227+yosrajalali@users.noreply.github.com>
2023-05-22 19:11:09 +0330

    add openedBox state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
-import Container from './Container';
-
-import RatingComponent from './RatingComponent';
-import Thanks from './Thanks';
+import Question from './Question';
+import woman from './images/illustration-woman-online-mobile.svg';
+import datas from './data';
+import deskWoman from './images/illustration-woman-online-desktop.svg';
+import box from './images/illustration-box-desktop.svg';
+import { useState } from 'react';
 
 function App() {
-  const [index, setIndex] = useState(0);
-  const [submited, setSubmited] = useState(false);
+  const [openedBox, setOpenedBox] = useState(null);
 
-  const handleSubmit = (index) => {
-    setIndex(index);
-    setSubmited(true);
-  };
+  const renderedData = datas.map((data) => {
+    return (
+      <div key={data.id}>
+        <Question
+          data={data}
+          openedBox={openedBox}
+          setOpenedBox={setOpenedBox}
+        />
+        <div className="line"></div>
+      </div>
+    );
+  });
 
   return (
-    <section>
-      {submited ? (
-        <Thanks index={index} />
-      ) : (
-        <RatingComponent onSubmit={handleSubmit} />
-      )}
-    </section>
+    <main>
+      <div className="accordion">
+        <div className="img-container">
+          <img
+            className="mobile-woman"
+            src={woman}
+            alt="woman working with a computer"
+          />
+          <img
+            className="desk-woman"
+            src={deskWoman}
+            alt="woman working with a computer"
+          />
+          <img src={box} alt="box" className="box" />
+        </div>
+        <div className="content">
+          <h1 className="faq">FAQ</h1>
+          <div className="questions">{renderedData}</div>
+        </div>
+      </div>
+    </main>
   );
 }
 
